4d20e3a20bbb5563bed999e4f3865249c1aa190c
Kaua Lourenzon <lourenzonkaua@gmail.com>
2021-08-16 08:16:59 -0300

    desenhoSub

--- CalculadoraDeSubtrair/frmSubtracao.Designer.cs
+++ CalculadoraDeSubtrair/frmSubtracao.Designer.cs
@@ -28,20 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnSubtrair = new System.Windows.Forms.Button();
+            this.txtSub2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSub1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(27, 99);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(124, 24);
+            this.lblResultado.TabIndex = 11;
+            this.lblResultado.Text = "RESULTADO";
+            // 
+            // btnSubtrair
+            // 
+            this.btnSubtrair.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubtrair.Location = new System.Drawing.Point(12, 70);
+            this.btnSubtrair.Name = "btnSubtrair";
+            this.btnSubtrair.Size = new System.Drawing.Size(157, 26);
+            this.btnSubtrair.TabIndex = 10;
+            this.btnSubtrair.Text = "-";
+            this.btnSubtrair.UseVisualStyleBackColor = true;
+            // 
+            // txtSub2
+            // 
+            this.txtSub2.Location = new System.Drawing.Point(79, 41);
+            this.txtSub2.Name = "txtSub2";
+            this.txtSub2.Size = new System.Drawing.Size(91, 20);
+            this.txtSub2.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "NÚMERO 2";
+            // 
+            // txtSub1
+            // 
+            this.txtSub1.Location = new System.Drawing.Point(79, 12);
+            this.txtSub1.Name = "txtSub1";
+            this.txtSub1.Size = new System.Drawing.Size(92, 20);
+            this.txtSub1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "NÚMERO 1";
             // 
             // frmSubtracao
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(178, 311);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnSubtrair);
+            this.Controls.Add(this.txtSub2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtSub1);
+            this.Controls.Add(this.label1);
             this.Name = "frmSubtracao";
             this.Text = "Subtração";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnSubtrair;
+        private System.Windows.Forms.TextBox txtSub2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSub1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
